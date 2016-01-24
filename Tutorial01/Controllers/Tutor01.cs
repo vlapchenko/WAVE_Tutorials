b039e980f748304a4ec0b831d9f4ae44f2df2ccb
vlapchenko <vlr.lapchenko@gmail.com>
2016-01-24 16:48:13 +0300

    Initial commit.

---
+++ Tutorial01/Controllers/Tutor01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NFX.Wave.MVC;
+using Tutorial01.Pages;
+
+namespace Tutorial01.Controllers
+{
+    public class Tutor01 : Controller
+    {
+        [Action]
+        public object Index()
+        {
+            return new Index();
+        }
+    }
+}
---
+++ Tutorial01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using NFX.ApplicationModel;
+using NFX.Wave;
+
+namespace Tutorial01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var app = new ServiceBaseApplication(args, null))
+                using (var server = new WaveServer())
+                {
+                    server.Configure(null);
+                    server.Start();
+                    Console.WriteLine("server started...");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Critical error:");
+                Console.WriteLine(ex);
+                Console.ReadLine();
+                Environment.ExitCode = -1;
+            }
+        }
+    }
+}
